401e1308a69ac40b9b0e7984408ed63b427f3fd8
Soumik15630m <soumik15630m@gmail.com>
2025-11-08 16:20:01 +0530

    See the sun!!

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,7 +2,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Get your URL and Key from the Supabase dashboard
-const supabaseUrl = 'https://pxgkwynzrvaihnjkgsvs.supabase.co'
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4Z2t3eW56cnZhaWhuamtnc3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTQxMzYsImV4cCI6MjA3NzQ5MDEzNn0.ZAvSoRFD6-2QrxauPz9x6mYSGMn4ABPNdt3tqiB8M6g"
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
